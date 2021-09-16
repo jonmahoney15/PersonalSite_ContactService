3ad03b979734c93fe1f9faaca1e6127207cb501e
jonmahoney15 <jonmahoney15@yahoo.com>
2021-09-16 15:24:09 -0400

    oops

--- src/Routes/Router.ts
+++ src/Routes/Router.ts
@@ -1,9 +1,10 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { SendEmail } from "../contact/ContactController";
 import { auth, rateLimiter } from "../Middleware";
 
 const router = Router();
 
-router.get("/contact/Contact", auth, (req, res) =>
+router.get("/contact/Contact", auth, (req: Request, res: Response) =>
   res.status(200).json({ Status: "Success", message: "Contact is Healthy" })
 );
 
