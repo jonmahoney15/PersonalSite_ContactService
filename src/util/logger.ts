6f4372c1ca48f8ec609bb17b46b01b299f0d8a5b
Jon Mahoney <jonmahoney15@yahoo.com>
2022-08-21 15:00:14 -0400

    Fix the build process for CI and Package Upgrade
    Upgraded packages and changed logger to new package

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,7 @@
 import pino from "pino";
 
 export const logger = pino({
-  level: "info",
-  prettyPrint: process.env.NODE_ENV !== "production",
+  transport: {
+    target: 'pino-pretty'
+  },
 });
